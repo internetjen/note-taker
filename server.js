77ddd70e299451f582d297737c72b27b7a39333f
Jennifer Arboleda <arboleda.jen@gmail.com>
2022-12-13 23:57:41 -0600

    updated db.json to be empty object

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const PORT = 3001;
 
 const app = express();
 
+//middleware
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
+//ROUTING
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
