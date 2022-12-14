17a14758a5dd41ea558273e35e9afa52d430bf48
Jennifer Arboleda <arboleda.jen@gmail.com>
2022-12-13 21:56:22 -0600

    added uuid to each note for deletion purposes. Notes typed into note taker are being saved on db.json with appropriate id

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const notesData = require('./db/db.json');
 const fs = require('fs');
 const uuid = require('./helpers/uuid'); // Helper method for generating unique ids
 
-// uuid(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-
 const PORT = 3001;
 
 const app = express();
@@ -29,31 +27,52 @@ app.get('/api/notes', (req, res) => res.json(notesData));
 
 // POST request to add a note
 app.post('/api/notes', (req, res) => {
-  const noteDataString = JSON.stringify(req.body);
+  // const noteDataString = JSON.stringify(req.body);
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
 
-  // Obtain existing notes
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      // Convert string into JSON object
-      const parsedNotes = JSON.parse(data);
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
 
-      // Add a new note
-      parsedNotes.push(req.body);
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
 
-      // Write updated notes back to the file
-      fs.writeFile(
-        './db/db.json',
-        JSON.stringify(parsedNotes),
-        (writeErr) =>
-          writeErr
-            ? console.error(writeErr)
-            : console.info('Successfully updated notes!')
-      );
-    }
-  })
-  res.json(notesData);
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
 });
 
 app.listen(PORT, () => {
